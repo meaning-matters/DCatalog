4aba3a819d376072710a88078e947c77ef0eaba8
Cornelis van der Bent <cornelis@meaning-matters.com>
2020-01-11 15:45:17 +0100

    Initial Commit

---
+++ DCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DCatalog
+//
+//  Created by Cornelis van der Bent on 11/01/2020.
+//  Copyright © 2020 Digidentity B.V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
